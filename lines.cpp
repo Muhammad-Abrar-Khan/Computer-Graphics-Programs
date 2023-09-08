6bc4ccfb5727f3c66c2080d8881a95c8ee1e02a0
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-09-08 23:54:52 +0500

    another commit

--- lines.cpp
+++ lines.cpp
@@ -4,11 +4,12 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0, 1.0, 1.0); // Set the color to white
 
-    glPointSize(10.0); // Increase the point size
+    glBegin(GL_LINES);
+    glVertex2f(-0.5, 0.0); // Start of the first line
+    glVertex2f(0.5, 0.0);  // End of the first line
 
-    glBegin(GL_POINTS);
-    glVertex2f(-0.5, 0.0); // First point
-    glVertex2f(0.5, 0.0);  // Second point
+    glVertex2f(0.0, -0.5); // Start of the second line
+    glVertex2f(0.0, 0.5);  // End of the second line
     glEnd();
 
     glFlush();
@@ -18,7 +19,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(640, 480);
-    glutCreateWindow("OpenGL Points");
+    glutCreateWindow("OpenGL Lines");
 
     glutDisplayFunc(display);
 
